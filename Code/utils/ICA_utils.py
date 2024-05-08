974565b7ffe11938f1c2867ddd81d9ebe64c60c8
NicoPietrusco <nicolopietro.pisanu@gmail.com>
2024-05-08 19:38:14 +0200

    3rd SCENARIO: Jazz-club Jam

--- Code/utils/ICA_utils.py
+++ Code/utils/ICA_utils.py
@@ -1,10 +1,9 @@
-
+import os
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy import signal
+from scipy.io import wavfile
 import sounddevice as sd
 from IPython.display import display, Audio
-
 
 
 def preprocessing(X, n_sources, under_complete = True):
@@ -44,8 +43,6 @@ def preprocessing(X, n_sources, under_complete = True):
     T_norm = (T) / T_std[:, None]
 
     return T_norm
-
-
 
 
 def FastIca(mixture_signals, n_sources, n_iter = 5000, tol = 1e-9, under_complete = True):
@@ -118,6 +115,61 @@ def FastIca(mixture_signals, n_sources, n_iter = 5000, tol = 1e-9, under_complet
     return S, W     
 
 
+def import_audio_folder(folder_path, duration=None):
+    """
+    Import audio files from a specified folder, normalize them, and ensure they have the same duration and sampling frequency.
+
+    Parameters:
+    - folder_path (str): Path to the folder containing audio files.
+    - duration (float): Desired duration in seconds for all samples (default: None).
+
+    Returns:
+    - processed_audio_data (numpy.ndarray): Normalized audio samples in a numpy array.
+    - sampling_frequency (int): Sampling frequency of the audio files.
+    - file_names (list): List of file names in the folder.
+    - length (samples): sample length of all audio samples after the importing and trimming.
+    """
+
+    # Find all the audio files in the folder
+    file_names = [file for file in os.listdir(folder_path) if file.endswith(".wav")]
+
+    # Initialize variables to store audio data and sampling frequencies
+    audio_data = []
+    sampling_frequencies = []
+
+    # Iterate over each audio file
+    for audio_file in file_names:
+        # Load the file path for the current audio file
+        file_path = os.path.join(folder_path, audio_file)
+
+        # Read the audio file
+        sampling_frequency, audio = wavfile.read(file_path)
+
+        # Check if duration is specified and trim audio if necessary
+        if duration is not None and len(audio) > duration * sampling_frequency:
+            audio = audio[:int(duration * sampling_frequency)]
+
+        # Normalize the audio sample
+        audio = audio / np.max(np.abs(audio))
+
+        # Store audio data and sampling frequency
+        audio_data.append(audio)
+        sampling_frequencies.append(sampling_frequency)
+
+    # Check if all files have the same sampling frequency
+    if len(set(sampling_frequencies)) != 1:
+        raise ValueError("Not all files have the same sampling frequency.")
+
+    # Ensure all audio samples have the same length
+    length = min(len(audio) for audio in audio_data)
+    processed_audio_data = np.array([audio[:length] for audio in audio_data])
+
+    # Remove ".wav" extension from names
+    file_names = [name.replace('.wav', '') for name in file_names]
+
+    return processed_audio_data, sampling_frequencies[0], file_names, length
+
+
 def norm_signals(signals_np, start=0, end=None, epsilon=1e-10): 
     """
     Normalize signals in a NumPy array by dividing each signal by its maximum absolute value.
@@ -148,8 +200,6 @@ def norm_signals(signals_np, start=0, end=None, epsilon=1e-10):
         signals_normalized = signals_np[:, start:end] / max_values
 
     return signals_normalized
-
-
 
 
 def plot_signals(t, S, title, labels = False, scaling = 1, xlim = 30, linewidth = 4): 
@@ -188,8 +238,6 @@ def plot_signals(t, S, title, labels = False, scaling = 1, xlim = 30, linewidth 
         plt.legend()
 
 
-
-
 def plot_signals_sequential(t, X, type, scaling = 1, xlim = 30, linewidth = 4):
     """
     Plot the mixed signals sequentially.
@@ -218,8 +266,6 @@ def plot_signals_sequential(t, X, type, scaling = 1, xlim = 30, linewidth = 4):
         # Setting limits for the axes
         axs[i].set_xlim(0, xlim)
         axs[i].set_ylim(-1*scaling, 1*scaling)
-
-
 
 
 def complete_plot(signals_np, fs, names='Signal', start=0, end=None, show_waveform=True, show_spectrogram=True, show_histogram=True, fmax=5000):
@@ -296,6 +342,46 @@ def complete_plot(signals_np, fs, names='Signal', start=0, end=None, show_wavefo
     plt.show()
 
 
+def plot_overlapped_signals(original_signals, reconstructed_signals, names, fs):
+    """
+    Plot overlapped original and reconstructed signals.
+
+    Parameters:
+        original_signals (list of arrays): List containing original signals.
+        reconstructed_signals (list of arrays): List containing reconstructed signals.
+        names (list of str): List of names for each signal.
+        fs (float): Sampling frequency.
+
+    Returns:
+        None
+    """
+    # Calculate time axis in seconds
+    time_sec = np.arange(len(original_signals[0])) / fs
+
+    # Create subplots for each pair of signals
+    fig, axs = plt.subplots(nrows=len(original_signals), ncols=1, figsize=(10, 3 * len(original_signals)),
+                            sharex=True, sharey=True)
+
+    # Plot original and reconstructed signals in each subplot
+    for i, (original, reconstructed) in enumerate(zip(original_signals, reconstructed_signals)):
+        axs[i].plot(time_sec, original, label="Original", color='blue', alpha=0.8)  # Plot original signal
+        axs[i].plot(time_sec, reconstructed, label="Reconstructed", color='orange', alpha=0.8)  # Plot reconstructed signal
+        axs[i].set_title(f"{names[i]}")  # Set title for the subplot
+        axs[i].grid(True)  # Add grid to the subplot
+        axs[i].tick_params(axis='both', which='major', labelsize=8)  # Lower font size of major ticks
+        axs[i].tick_params(axis='both', which='minor', labelsize=4)  # Lower font size of minor ticks
+        axs[i].set_xlabel('Time [s]', fontsize=10)  # Lower font size of x-axis label
+        axs[i].set_ylabel('Amplitude', fontsize=10)  # Lower font size of y-axis label
+
+    # Create a single legend for all subplots
+    handles, labels = axs[0].get_legend_handles_labels()
+    fig.legend(handles, labels, loc='upper right')
+
+    plt.suptitle('Overlapped Signals')  # Overall title for the plot
+    plt.tight_layout()  # Adjust layout to prevent overlapping of subplots
+    plt.show()  # Display the plot
+
+
 def play_audio_from_array(audio_np, samplerate=44100):
     """
     Play audio from a NumPy array.
@@ -311,8 +397,7 @@ def play_audio_from_array(audio_np, samplerate=44100):
     sd.wait()
 
 
-
-def audio_widget(audio_np, samplerate, name='audio'):
+def audio_widget(audio_np, samplerate, names='audio'):
     """
     Create Audio widgets from a 2D NumPy array of audio sources.
 
@@ -321,14 +406,14 @@ def audio_widget(audio_np, samplerate, name='audio'):
     - samplerate (int): Sample rate of the audio sources.
     - name (str or list): Name prefix for the audio widgets. If it's a list, each audio source will be named accordingly.
     """
-    if isinstance(name, list):
-        assert len(name) == audio_np.shape[0], "Number of names should match the number of audio sources."
+    if isinstance(names, list):
+        assert len(names) == audio_np.shape[0], "Number of names should match the number of audio sources."
         for i, audio in enumerate(audio_np):
-            print(f'{name[i]}:')
+            print(f'{names[i]}:')
             display(Audio(audio, rate=samplerate))
     else:
         for i, audio in enumerate(audio_np):
-            print(f'{name} {i+1}:')
+            print(f'{names} {i+1}:')
             display(Audio(audio, rate=samplerate))
 
 
